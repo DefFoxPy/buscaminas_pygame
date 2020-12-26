8956e00afca29f2ba5ba92e3ed0d0c97920e8658
juanmpy05 <66576421+juanmpy05@users.noreply.github.com>
2020-12-26 16:23:53 -0400

    determina cuando el jugador gana o pierde
    Cambios menores
    * eliminación de algunos comentarios

--- game/game.py
+++ game/game.py
@@ -6,7 +6,6 @@ from .config import *
 from .tablero import Tablero
 from .helper import *
 
-
 class Game:
 
 	def __init__(self):
@@ -25,17 +24,14 @@ class Game:
 		self.dir_images = os.path.join(self.dir, 'sources/image')
 
 	def start(self):
-		''' da comienzo al juego '''
 		self.new()
 
 	def new(self):
-		''' Inicializa una instancia del juego '''
 		self.tablero = Tablero()
 		self.game_over = False
 		self.run()
 
 	def run(self):
-		''' ciclo principal del juego '''
 		while self.running:
 			self.events() 
 			self.update() 
@@ -43,37 +39,44 @@ class Game:
 			self.clock.tick(FPS)
 
 	def events(self):
-		''' Manejo de todos los eventos del juego '''
 		for event in pygame.event.get():
-			# El usuario presionó la tecla de salir
+		
 			if event.type == pygame.QUIT:  
 				self.running = False
 				pygame.quit()
 				sys.exit()
-			# El usuario precionó una casillas
+			
 			if event.type == pygame.MOUSEBUTTONDOWN:
 				if not self.game_over:
-					# obtenemos el equivalente en x, y de la posicion del mouse
+					
 					x, y = pixeles_a_indice(pygame.mouse.get_pos())
-					# en caso de que el usuario tecleo fuera del tablero salimos del ciclo
+					
 					if not coordenada_valida(x, y): continue
 
 					if event.button == CLIC_IZQUIERDO:
 						if not self.tablero.get_bandera(x, y): 
-							self.tablero.liberar(x, y) # to do
+							self.tablero.liberar(x, y)
 							self.tablero.set_visible(x, y, True)
+							
+							if self.tablero.es_mina_detonada(x, y):
+								print('#DEBUG: el jugador perdió')
+								self.stop()
+
+							if self.tablero.todas_casillas_liberadas():
+								print('#DEBUG: el jugador ganó')
+								self.stop()
 
 					if event.button == CLIC_DERECHO:
 						if not self.tablero.get_visible(x, y):
 							self.tablero.cambiar_bandera(x, y)	
 
 	def update(self):
-		'''Actualiza cada fotograma del juego'''
 		pass
 
+	def stop(self):
+		self.game_over = True
 
 	def draw(self):
-		'''Dibuja todos los elemenentos en pantalla'''
 		self.surface.fill(WHITE)
 		self.tablero.mostrar(self.surface, self.dir_images)
 		pygame.display.update()
--- game/tablero.py
+++ game/tablero.py
@@ -7,7 +7,6 @@ from .helper import *
 class Tablero:
 
 	def __init__(self):
-		""" """
 		self.t = list()
 		self.crear()
 		self.agregar_minas()
@@ -25,14 +24,7 @@ class Tablero:
 				casilla = Casilla(x, y)
 				self.t[x].append(casilla)
 
-	def mostrar(self, surface, dir_images):
-		""" Muestra cada casilla dentro del tablero """
-		for x in range(COLUMNA):
-			for y in range(FILA):
-				self.t[x][y].mostrar(surface, dir_images)
-
 	def agregar_minas(self):
-		""" por ahora agrega 10 minas de manera aleatorea """
 		from random import randint
 
 		minas_agregadas = 0
@@ -46,11 +38,12 @@ class Tablero:
 				minas_agregadas += 1
 
 	def agregar_numeros(self):
-		""" """
+		""" determina cual será el contenido de cada casilla según las minas se sus casillas vecinas """
 		for x in range(COLUMNA):
 			for y in range(FILA):
 				
 				if self.t[x][y].get_contenido() == CASILLA_MINA: continue
+				
 				numero_minas = 0
 
 				if coordenada_valida(x-1, y-1) and self.t[x-1][y-1].get_contenido() == CASILLA_MINA:
@@ -79,8 +72,14 @@ class Tablero:
 
 				self.t[x][y].set_contenido(numero_minas)
 
+	def mostrar(self, surface, dir_images):
+		""" Dibuja el tablero en pantalla """
+		for x in range(COLUMNA):
+			for y in range(FILA):
+				self.t[x][y].mostrar(surface, dir_images)
+
 	def liberar(self, x, y):
-		""" """
+		"""  """
 		# Casos bases
 		if not coordenada_valida(x, y): return
 		if self.t[x][y].get_visible(): return
@@ -103,11 +102,31 @@ class Tablero:
 		self.liberar(x, y+1)
 		self.liberar(x-1, y+1)
 
+	def es_mina_detonada(self, x, y):
+		""" determina si en una casilla recien descubierta habia una mina """
+		return self.t[x][y].get_contenido() == CASILLA_MINA
 
+	def todas_casillas_liberadas(self):
+		""" determina si ya el usuario ha despejado todas las casillas sin minas """
+		despejado = True # asume que las casilla están despejadas
+		for x in range(COLUMNA):
+			for y in range(FILA):
+				# obtenemos los datos de la casilla
+				visible = self.t[x][y].get_visible()
+				contenido = self.t[x][y].get_contenido()
+
+				if not visible and contenido != CASILLA_MINA:
+					despejado = False # aún faltan minas
+
+		return despejado
 
 	def get_bandera(self, x, y):
 		""" obtiene el estado del atributo bandera de una casilla en particular """
 		return self.t[x][y].get_bandera()
+
+	def get_contenido(self, x, y):
+		""" Obtiene el contenido de la casilla correspondiente """
+		return self.t[x][y].get_contenido()
 
 	def get_visible(self, x, y):
 		""" obtiene el estado del atributo 'visible' de una casilla en particular """
